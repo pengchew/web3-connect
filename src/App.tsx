9876c43d2c75bf0250826dfaaeb82b411bf66a76
ChowPengChew <48185838+ChowPengChew@users.noreply.github.com>
2021-10-19 05:24:30 +0800

    Added polygon switch

--- src/App.tsx
+++ src/App.tsx
@@ -14,46 +14,95 @@ declare let window: any;
 
 function App() {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const[provider,setprovider]=useState<{
+    web3:any,
+    address:string,
+    shortAddress:string,
+    networkId:number|string,
+    weiBalance:number|string,
+    etherBalance:number|string,
+    symbol:string,
+    jazzicon:any,
+  }>({
+    web3:'',
+    address:'',
+    shortAddress:'',
+    networkId:'',
+    weiBalance:'',
+    etherBalance:'',
+    symbol:'MATIC',
+    jazzicon:<></>
+  })
+
+  function handlesetprovider(input=provider){
+      setprovider(input);
+  }
+
+  const StyledIdenticon = styled.div`
+    height: 1rem;
+    width: 1rem;
+    border-radius: 1.125rem;
+    display:inline-block;
+    margin-left:0.5rem;
+  `;
+  async function getProvider(flag={disconnect:false }) {
+
+      const web3:any = new Web3(window.ethereum)
+      await window.ethereum.enable();
+      let _address = web3.utils.toChecksumAddress((await web3.eth.getAccounts())[0]);
+      let _networkId = await web3.eth.net.getId();
+      let _wei = '';
+      let _eth = '';
+
+
+      await web3.eth.getBalance(_address,(err:any,res:any)=>{
+         _wei=res as string;
+         _eth=web3.utils.fromWei(res,'ether');
+      })
+
+      const _provider=flag.disconnect?{
+        web3:web3,
+        address:'',
+        shortAddress:'',
+        networkId:'',
+        weiBalance:'',
+        etherBalance:'',
+        symbol:provider.symbol,
+        jazzicon:<></>
+      }:{
+        web3:web3,
+        address:_address,
+        shortAddress:_address.slice(0, 6)+'...'+_address.slice(_address.length - 4,_address.length),
+        networkId:_networkId,
+        weiBalance:_wei,
+        etherBalance:_eth,
+        symbol:provider.symbol,
+        jazzicon:<StyledIdenticon ref={(nodeElement) => {
+                                    if (nodeElement) {
+                                      nodeElement.innerHTML = ''
+                                      nodeElement.appendChild(Jazzicon(16, parseInt(_address.slice(2, 10), 16)))
+                                    }
+                                  }}
+                                ></StyledIdenticon>
+      }
+      setprovider(_provider);
+      console.log(_provider);
+  }
+  useEffect(()=>{
+    window.ethereum.on("accountsChanged", async () => {
+      getProvider()
+    });
+    window.ethereum.on("chainChanged", async () => {
+      getProvider()
+    });
+  },[])
   return (
     <ChakraProvider theme={theme}>
       <Layout>
-        <ConnectButton isOpen={isOpen} onClose={onClose} handleOpenModal={onOpen} />
+        <ConnectButton provider={provider} getProvider={getProvider} isOpen={isOpen} onClose={onClose} handleOpenModal={onOpen} />
       </Layout>
     </ChakraProvider>
   );
 }
 
 export default App;
-
-const StyledIdenticon = styled.div`
-  height: 1rem;
-  width: 1rem;
-  border-radius: 1.125rem;
-  background-color: black;
-`;
-export async function getProvider() {
-    const web3:any = new Web3(window.ethereum)
-    await window.ethereum.enable();
-    let _address = web3.utils.toChecksumAddress((await web3.eth.getAccounts())[0]);
-    let _networkId = await web3.eth.net.getId();
-    let _wei = '';
-    let _eth = '';
-
-    await web3.eth.getBalance(_address,(err:any,res:any)=>{
-       _wei=res as string;
-       _eth=web3.utils.fromWei(res,'ether');
-    })
-    return{
-      'address':_address,
-      'networkId':_networkId,
-      'weiBalance':_wei,
-      'etherBalance':_eth,
-      'jazzicon':<div ref={(nodeElement) => {
-                      if (nodeElement) {
-                        nodeElement.innerHTML = ''
-                        nodeElement.appendChild(Jazzicon(16, parseInt(_address.slice(2, 10), 16)))
-                      }
-                    }}
-                  ></div>
-    }
-}
--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,6 +1,5 @@
 import Web3 from 'web3';
-import { useEffect, useState } from "react";
-import { getProvider } from "../App";
+import { useEffect, useState,useRef } from "react";
 import {
   Box,
   Button,
@@ -18,227 +17,288 @@ import {
 import { ExternalLinkIcon, CopyIcon } from "@chakra-ui/icons";
 
 
-
-
 declare let window: any;
 
+export async function changeNetwork(network=''){
+  enum ChainId {
+    MAINNET = 1,
+    ROPSTEN = 3,
+    RINKEBY = 4,
+    GOERLI = 5,
+    KOVAN = 42,
+
+    ARBITRUM_ONE = 42161,
+    ARBITRUM_RINKEBY = 421611,
+    OPTIMISM = 10,
+    OPTIMISTIC_KOVAN = 69,
+  }
+  const CHAIN_INFO: { [key: string]: any } = {
+    'BNB':{
+      chainId: '0x38',
+      chainName: 'Binance Smart Chain',
+      nativeCurrency: {
+          name: 'Binance Coin',
+          symbol: 'BNB',
+          decimals: 18
+      },
+      rpcUrls: ['https://bsc-dataseed.binance.org/'],
+      blockExplorerUrls: ['https://bscscan.com/']
+    },
+    'FTM':{
+      chainId: '0xfa',
+      chainName: 'Fantom Opera',
+      nativeCurrency: {
+          name: 'FTM',
+          symbol: 'FTM',
+          decimals: 18
+      },
+      rpcUrls: ['https://rpc.ftm.tools/'],
+      blockExplorerUrls: ['https://ftmscan.com/']
+    },
+    'MATIC':{
+      chainId: '0x89',
+      chainName: 'Matic Mainnet',
+      nativeCurrency: {
+          name: 'MATIC',
+          symbol: 'MATIC',
+          decimals: 18
+      },
+      rpcUrls: ['https://rpc-mainnet.maticvigil.com/'],
+      blockExplorerUrls: ['https://explorer.matic.network/']
+    },
+    'AVAX':{
+      chainId: '0xa86a',
+      chainName: 'Avalanche Network',
+      nativeCurrency: {
+          name: 'AVAX',
+          symbol: 'AVAX',
+          decimals: 18
+      },
+      rpcUrls: ['https://api.avax.network/ext/bc/C/rpc/'],
+      blockExplorerUrls: ['https://cchain.explorer.avax.network/']
+    },
+
+  }
+
+  const tx = await window.ethereum.request({method: 'wallet_addEthereumChain', params:[CHAIN_INFO[network]]}).catch()
+  if (tx) {
+      console.log(tx)
+  }
+}
 type Props = {
   handleOpenModal: any;
   isOpen: any;
   onClose: any;
+  provider: any;
+  getProvider: any;
 };
 
-export default function ConnectButton({ handleOpenModal,isOpen, onClose }: Props) {
+export default function ConnectButton({ handleOpenModal,isOpen, onClose,provider,getProvider }: Props) {
   const web3:any = new Web3(window.ethereum)
   // const provider = connect();
+  const initialRef = useRef<any>();
 
-  const[provider,setprovider]=useState<{
-    address:any,
-    networkId:any,
-    weiBalance:number|string,
-    etherBalance:number|string,
-    jazzicon:any,
-  }>({
-    address:'',
-    networkId:'',
-    weiBalance:'',
-    etherBalance:'',
-    jazzicon:<></>
-  })
-  async function handleConnectWallet() {
-      connect()
-  }
-  async function connect() {
-      setprovider(await getProvider())
-  }
-
-
-  useEffect(()=>{
-    window.ethereum.on("accountsChanged", async () => {
-      setprovider({...await getProvider()})
-    });
-    window.ethereum.on("chainChanged", async () => {
-      setprovider({...await getProvider()})
-    });
-  },[])
-  return provider.address ? (
-    <><Modal isOpen={isOpen} onClose={onClose} isCentered size="md">
-      <ModalOverlay />
-      <ModalContent
-        background="gray.900"
-        border="1px"
-        borderStyle="solid"
-        borderColor="gray.700"
-        borderRadius="3xl"
-      >
-        <ModalHeader color="white" px={4} fontSize="lg" fontWeight="medium">
-          Account
-        </ModalHeader>
-        <ModalCloseButton
-          color="white"
-          fontSize="sm"
-          _hover={{
-            color: "whiteAlpha.700",
-          }}
-        />
-        <ModalBody pt={0} px={4}>
-          <Box
-            borderRadius="3xl"
-            border="1px"
-            borderStyle="solid"
-            borderColor="gray.600"
-            px={5}
-            pt={4}
-            pb={2}
-            mb={3}
-          >
-            <Flex justifyContent="space-between" alignItems="center" mb={3}>
-              <Text color="gray.400" fontSize="sm">
-                Connected with MetaMask
-              </Text>
-              <Button
-                variant="outline"
-                size="sm"
-                borderColor="blue.800"
-                borderRadius="3xl"
-                color="blue.500"
-                fontSize="13px"
-                fontWeight="normal"
-                px={2}
-                height="26px"
-                _hover={{
-                  background: "none",
-                  borderColor: "blue.300",
-                  textDecoration: "none",
-                }}
-              >
-                Change
-              </Button>
-            </Flex>
-            <Flex alignItems="center" mt={2} mb={4} lineHeight={1}>
-
-              <Text
-                color="white"
-                fontSize="xl"
-                fontWeight="semibold"
-                ml="2"
-                lineHeight="1.1"
-              >
-                {provider.address &&
-                  `${provider.address.slice(0, 6)}...${provider.address.slice(
-                    provider.address.length - 4,
-                    provider.address.length
-                  )}`}
-              </Text>{provider.jazzicon}
-            </Flex>
-            <Flex alignContent="center" m={3}>
-              <Button
-                variant="link"
-                color="gray.400"
-                fontWeight="normal"
-                fontSize="sm"
-                _hover={{
-                  textDecoration: "none",
-                  color: "whiteAlpha.800",
-                }}
-              >
-                <CopyIcon mr={1} />
-                Copy Address
-              </Button>
-              <Link
-                fontSize="sm"
-                display="flex"
-                alignItems="center"
-                href={`https://ropsten.etherscan.io/address/${provider.address}`}
-                isExternal
-                color="gray.400"
-                ml={6}
-                _hover={{
-                  color: "whiteAlpha.800",
-                  textDecoration: "underline",
-                }}
-              >
-                <ExternalLinkIcon mr={1} />
-                View on Explorer
-              </Link>
-            </Flex>
-          </Box>
-        </ModalBody>
-
-        <ModalFooter
-          justifyContent="end"
-          background="gray.700"
-          borderBottomLeftRadius="3xl"
-          borderBottomRightRadius="3xl"
-          p={6}
-        >
-          <Text
-            color="white"
-            textAlign="left"
-            fontWeight="medium"
-            fontSize="md"
-          >
-            Your transactions willl appear here...
-          </Text>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
-    <Box
-      display="flex"
-      alignItems="center"
-      background="gray.700"
-      borderRadius="xl"
-      py="0"
-    >{}
-      <Box px="3">
-        <Text color="white" fontSize="md">
-          {provider.etherBalance} ETH
-        </Text>
-      </Box>
-      <Button
-        onClick={handleOpenModal}
-        bg="gray.800"
+  if(!provider.address){
+    return (
+      <><Button
+        onClick={()=>getProvider({})}
+        bg="blue.800"
+        color="blue.300"
+        fontSize="lg"
+        fontWeight="medium"
+        borderRadius="xl"
         border="1px solid transparent"
         _hover={{
-          border: "1px",
-          borderStyle: "solid",
-          borderColor: "blue.400",
-          backgroundColor: "gray.700",
+          borderColor: "blue.700",
+          color: "blue.400",
         }}
-        borderRadius="xl"
-        m="1px"
-        px={3}
-        height="38px"
+        _active={{
+          backgroundColor: "blue.800",
+          borderColor: "blue.700",
+        }}
       >
-        <Text color="white" fontSize="md" fontWeight="medium" mr="2">
-          {provider.address &&
-            `${provider.address.slice(0, 6)}...${provider.address.slice(
-              provider.address.length - 4,
-              provider.address.length
-            )}`}
-        </Text>
-        {provider.jazzicon}
-      </Button>
-    </Box></>
-  ) : (
-    <Button
-      onClick={handleConnectWallet}
-      bg="blue.800"
-      color="blue.300"
-      fontSize="lg"
-      fontWeight="medium"
-      borderRadius="xl"
-      border="1px solid transparent"
-      _hover={{
-        borderColor: "blue.700",
-        color: "blue.400",
-      }}
-      _active={{
-        backgroundColor: "blue.800",
-        borderColor: "blue.700",
-      }}
-    >
-      Connect to a wallet
-    </Button>
-  );
+        Connect to a wallet
+      </Button></>
+    );
+  }
+
+  if(provider.networkId!==137){
+    return(
+      <><Button
+        onClick={()=>changeNetwork(provider.symbol)}
+        bg="blue.800"
+        color="blue.300"
+        fontSize="lg"
+        fontWeight="medium"
+        borderRadius="xl"
+        border="1px solid transparent"
+        _hover={{
+          borderColor: "blue.700",
+          color: "blue.400",
+        }}
+        _active={{
+          backgroundColor: "blue.800",
+          borderColor: "blue.700",
+        }}
+      >
+        Switch to {provider.symbol}
+      </Button></>
+    )
+  }
+  else{
+    return(
+      <><Modal autoFocus={false}  isOpen={isOpen} onClose={onClose} isCentered size="md">
+        <ModalOverlay />
+        <ModalContent
+          background="gray.900"
+          border="1px"
+          borderStyle="solid"
+          borderColor="gray.700"
+          borderRadius="3xl"
+        >
+          <ModalHeader color="white" px={4} fontSize="lg" fontWeight="medium">
+            Account
+
+          </ModalHeader>
+          <ModalCloseButton
+            color="white"
+            fontSize="sm"
+          />
+          <ModalBody pt={0} px={4}>
+            <Box
+              borderRadius="3xl"
+              border="1px"
+              borderStyle="solid"
+              borderColor="gray.600"
+              px={5}
+              pt={4}
+              pb={2}
+              mb={3}
+            >
+              <Flex justifyContent="space-between" alignItems="center" mb={3}>
+                <Text color="gray.400" fontSize="sm">
+                  Connected with MetaMask
+                </Text>
+                <Button
+                  ref={initialRef}
+                  onClick={()=>getProvider({disconnect:true})}
+                  variant="outline"
+                  size="sm"
+                  borderColor="blue.800"
+                  borderRadius="3xl"
+                  color="blue.500"
+                  fontSize="13px"
+                  fontWeight="normal"
+                  px={2}
+                  height="26px"
+                  _hover={{
+                    background: "none",
+                    borderColor: "blue.300",
+                    textDecoration: "none",
+                  }}
+                >
+                  Change
+                </Button>
+              </Flex>
+              <Flex alignItems="center" mt={2} mb={4} lineHeight={1}>
+
+                <Text
+                  color="white"
+                  fontSize="xl"
+                  fontWeight="semibold"
+                  ml="2"
+                  lineHeight="1.1"
+                >
+                  {provider.shortAddress}
+                </Text>{provider.jazzicon}
+              </Flex>
+              <Flex alignContent="center" m={3}>
+                <Button
+                  onClick={() => {navigator.clipboard.writeText(provider.address)}}
+                  variant="link"
+                  color="gray.400"
+                  fontWeight="normal"
+                  fontSize="sm"
+                  _hover={{
+                    textDecoration: "none",
+                    color: "whiteAlpha.800",
+                  }}
+                >
+                  <CopyIcon mr={1} />
+                  Copy Address
+                </Button>
+                <Link
+                  fontSize="sm"
+                  display="flex"
+                  alignItems="center"
+                  href={`https://polygonscan.com/address/${provider.address}`}
+                  isExternal
+                  color="gray.400"
+                  ml={6}
+                  _hover={{
+                    color: "whiteAlpha.800",
+                    textDecoration: "underline",
+                  }}
+                >
+                  <ExternalLinkIcon mr={1} />
+                  View on Explorer
+                </Link>
+              </Flex>
+            </Box>
+          </ModalBody>
+
+          <ModalFooter
+            justifyContent="end"
+            background="gray.700"
+            borderBottomLeftRadius="3xl"
+            borderBottomRightRadius="3xl"
+            p={6}
+          >
+            <Text
+              color="white"
+              textAlign="left"
+              fontWeight="medium"
+              fontSize="md"
+            >
+              Your transactions will appear here...
+            </Text>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+      <Box
+        display="flex"
+        alignItems="center"
+        background="gray.700"
+        borderRadius="xl"
+        py="0"
+      >{}
+        <Box px="3">
+          <Text color="white" fontSize="md">
+            {parseFloat(provider.etherBalance as string).toFixed(4)} {provider.symbol}
+          </Text>
+        </Box>
+        <Button
+          onClick={handleOpenModal}
+          bg="gray.800"
+          border="1px solid transparent"
+          _hover={{
+            border: "1px",
+            borderStyle: "solid",
+            borderColor: "blue.400",
+            backgroundColor: "gray.700",
+          }}
+          borderRadius="xl"
+          m="1px"
+          px={3}
+          height="38px"
+        >
+          <Text color="white" fontSize="md" fontWeight="medium" mr="2">
+            {provider.shortAddress}
+          </Text>
+          {provider.jazzicon}
+        </Button>
+      </Box></>
+    )
+  }
+
 }
